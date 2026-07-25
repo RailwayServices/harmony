dd26d323c2c0938efa6407047751e0a7febe8062
Waris <waris4ly@gmail.com>
2026-07-25 12:55:29 +0530

    Delete vendor/lavende-core directory

--- vendor/lavende-core/src/audio/error.rs
+++
@@ -1,8 +0,0 @@
-use thiserror::Error;
-#[derive(Debug, Error)]
-pub enum AudioError {
-    #[error("Decoder finished")]
-    DecoderFinished,
-    #[error("IO error: {0}")]
-    Io(#[from] std::io::Error),
-}
